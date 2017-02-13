54b174d555f66be6b0b91440a82388aad151bbe0
Valerie Boem <vboem@gmail.com>
2017-02-13 16:09:26 -0500

    Learned about functions

---
+++ pacman-function.js
@@ -0,0 +1,13 @@
+// Named function
+
+function eatDot() {
+  console.log("MUNCH");
+}
+
+//Anon function
+// Here we assign the function to a variable instead of giving it a name directly
+var eatDot = function() {
+  console.log("MUNCH");
+}
+
+eatDot();
